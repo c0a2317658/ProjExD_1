413a0f1a6c77666d7967352a072f32e0289ac25b
C0A23176 <c0a2317658@edu.teu.ac.jp>
2024-06-11 16:05:44 +0900

    300,200に変更

---
+++ pygame_image.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3png")
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        kk_rct = kk_img.grt_rect() #こうかトンレクとの中質
+        kk_rct.center = 300,200
+        screen.blit(kk_img, kk_rct) #kk_imgをkk_rctの設定に従って貼り付け
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
