d1338e203831306abddc2bfd8145a2cb9e8bc1c0
C0A23176 <c0a2317658@edu.teu.ac.jp>
2024-06-11 16:45:34 +0900

    練習8：押下キーに応じた高官と移動

--- pygame_image.py
+++ pygame_image.py
@@ -10,19 +10,37 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3png")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    
+    kk_rct = kk_img.get_rect() #こうかトンレクとの中質
+    kk_rct.center = 300,200
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        kk_rct = kk_img.grt_rect() #こうかトンレクとの中質
-        kk_rct.center = 300,200
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
+
+        key_list = pg.key.get_pressed() #全キー
+        if key_list[pg.K_UP]: #上矢印が押されたら
+            kk_rct.move_ip((0,-1))
+        if key_list[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_list[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_list[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
         screen.blit(kk_img, kk_rct) #kk_imgをkk_rctの設定に従って貼り付け
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
