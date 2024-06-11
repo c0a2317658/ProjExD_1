8aeaaa9e34b480b14f70788e5d8e54a4a98ebb51
C0A23176 <c0a2317658@edu.teu.ac.jp>
2024-06-11 15:28:28 +0900

    縦幅の変更

--- pygame_system.py
+++ pygame_system.py
@@ -7,12 +7,12 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はじめてのPygame") #タイトル
-    screen = pg.display.set_mode((800, 600)) #画面サイズ
+    screen = pg.display.set_mode((800, 500)) #画面サイズ800*600
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
 
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn = pg.Surface((20, 20)) #空のサーフェイス
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)#255（赤）
     enn.set_colorkey((0, 0, 0))
 
     tmr = 0
@@ -23,7 +23,7 @@ def main():
         txt = font.render(str(tmr), True, (255, 255, 255))
         screen.fill((50, 50, 50))
         screen.blit(txt, [300, 200])
-        screen.blit(enn, [100, 400])
+        screen.blit(enn, [100, 400]) #100,400の座標
         pg.display.update()
         tmr += 1        
         clock.tick(1)
